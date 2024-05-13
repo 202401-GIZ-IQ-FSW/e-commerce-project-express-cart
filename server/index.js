2b6cfee595ed5f70815b86f6c89bbdceb673d3ca
Ninos Dinkha <ninos.dinkha97@gmail.com>
2024-05-13 23:57:03 +0300

    added basic routes

--- server/index.js
+++ server/index.js
@@ -1,16 +1,18 @@
-const express = require("express");
-require("dotenv").config();
+const express = require('express');
+require('dotenv').config();
 
-const connectToMongo = require("./db/connection");
+const connectToMongo = require('./db/connection');
+const adminRoutes = require('./routes/admin');
+const customerRoutes = require('./routes/customer');
 
 const app = express();
-const port =
-  process.env.NODE_ENV === "test"
-    ? process.env.NODE_LOCAL_TEST_PORT
-    : process.env.NODE_LOCAL_PORT;
+const port = process.env.NODE_ENV === 'test' ? process.env.NODE_LOCAL_TEST_PORT : process.env.NODE_LOCAL_PORT;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+app.use('/admin', adminRoutes);
+app.use('/customer', customerRoutes);
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
