48040a9b60b2b78ff62ed3fedad542b880db25a1
Halwest Abubakir <halwest.east@gmail.com>
2024-05-14 21:17:52 +0300

    shop-item-model added

---
+++ server/models/shopItemmodel.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const shopItemSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Please add a name"],
+    },
+    image: {
+      type: String,
+      required: false,
+    },
+    price: {
+      type: Number,
+      required: [true, "Please set the price"],
+    },
+    description: {
+      type: String,
+      required: false,
+    },
+    availableCount: {
+      type: String,
+      required: [true, "Please set the available quantity"],
+    },
+    category: {
+      type: String,
+      required: [true, "Please add a category"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Shopitem", shopItemSchema);
