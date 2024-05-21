e6b3d9b7def0205a19f4f74d4f89e83425a7d220
aland <rebwar.aland11@gmail.com>
2024-05-21 23:29:32 +0300

    added ui route

--- server/index.js
+++ server/index.js
@@ -1,32 +1,40 @@
-const express = require('express');
-require('dotenv').config();
+const express = require("express");
+require("dotenv").config();
+
+const path = require("path");
 
 // config and middleware
-const connectToMongo = require('./db/connection');
-const cookieParser = require('cookie-parser');
-const verifyJWT = require('./middleware/verifyJWT');
-const verifyRoles = require('./middleware/verifyRoles');
+const connectToMongo = require("./db/connection");
+const cookieParser = require("cookie-parser");
+const verifyJWT = require("./middleware/verifyJWT");
+const verifyRoles = require("./middleware/verifyRoles");
 
 // routes
-const shopItemsRoutes = require('./routes/shopItems');
-const authRoutes = require('./routes/auth/auth');
-const adminRoutes = require('./routes/admin/admin');
-const customerRoutes = require('./routes/customer/customer');
-const USER_ROLES = require('./config/userRoles');
+const shopItemsRoutes = require("./routes/shopItems");
+const authRoutes = require("./routes/auth/auth");
+const adminRoutes = require("./routes/admin/admin");
+const customerRoutes = require("./routes/customer/customer");
+const uiRoutes = require("./routes/ui/ui");
+const USER_ROLES = require("./config/userRoles");
 
 const app = express();
-const port = process.env.NODE_ENV === 'test' ? process.env.NODE_LOCAL_TEST_PORT : process.env.NODE_LOCAL_PORT;
+const port = 3000;
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/shop-items', shopItemsRoutes);
-app.use('/auth', authRoutes);
+app.use("/ui", uiRoutes);
+
+app.use("/shop-items", shopItemsRoutes);
+app.use("/auth", authRoutes);
 
 app.use(verifyJWT); // everything below this line will use verifyJWT
-app.use('/customer', verifyRoles(USER_ROLES.Customer), customerRoutes);
-app.use('/admin', verifyRoles(USER_ROLES.Admin), adminRoutes);
+app.use("/customer", verifyRoles(USER_ROLES.Customer), customerRoutes);
+app.use("/admin", verifyRoles(USER_ROLES.Admin), adminRoutes);
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
