1248b3951f7106c981eb4c6296ebb2b4b98613db
Ninos Dinkha <ninos.dinkha97@gmail.com>
2024-05-22 00:22:34 +0300

    organized route folders

--- server/routes/customer/root.js
+++ server/routes/customer/root.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const cartRoutes = require('./cart');
-const customerAuthRoutes = require('../auth/customerAuth');
 
 const {
   handleCheckout,
